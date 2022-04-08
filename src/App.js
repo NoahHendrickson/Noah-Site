036427b7c638eee93a8d3955020fa8f79cfdfd0b
Noah Hendrickson <noahjames017@gmail.com>
2022-04-07 22:09:49 -0400

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import './FlowerLogo.css'
+import FlowerLogo from './FlowerLogo';
+import {ReactComponent as NoahLogo} from './Assets/NoahLogo-10.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="splash__page">
+        <FlowerLogo />
+        <NoahLogo className='NoahLogo'/>
+        <div className='splash__page--buttons button_container'>
+          <button>
+            <p>Design</p>
+            <div className='SlideOutMenu'>
+              <ul>
+                <li>UX</li>
+                <li>Graphic</li>
+                <li>Logo</li>
+              </ul>
+            </div>
+          </button>
+          <button>Code</button>
+          <button>Story</button>
+        </div>
+     </div>
     </div>
   );
 }
