eec3f032632cfd6da419ece35a16c2fc459b5a09
Noah Hendrickson <noahjames017@gmail.com>
2022-05-19 21:57:56 -0400

    fixes

--- src/FullStory.js
+++ src/FullStory.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import Navbar from "./Navbar"
 import "./FullStory.css"
-import { ReactComponent as DownCarat } from "./Assets/downCarat-18.svg"
+import { ReactComponent as DownCarat } from "./Assets/Story/fancyDownCarat.svg"
 
 const FullStory = () => {
   return (
@@ -9,18 +10,28 @@ const FullStory = () => {
       <Navbar />
       <div className='story__grid'>
         <div>
-          <h1 className='story__timeline'>Went to College for Graphic Design</h1>
-          <DownCarat className='downCarat' />
+          <motion.div whileHover={{scale: 1.05}} className='timeline__button'>
+            <div className='timeline__button--front'>
+            <h1 className='story__timeline'>Went to College for Graphic Design</h1>
+            </div>
+          </motion.div>
+            <motion.div whileHover={{scale: 1.2}}>
+            <DownCarat className='downCarat' />
+            </motion.div>
        </div>
         <div className='span__6'>
-          <h1 className='story__grid--title'>currently buidling this page!! its pretty harding to 1. not sound narcisstic and 2. make this stuff interesting so ppl want to read it 😬</h1>
+            <h1 className='story__grid--title'>currently buidling this page!! its pretty harding to 1. not sound narcisstic and 2. make this stuff interesting so ppl want to read it 😬</h1>
         </div>
         <div className='span__2'>
           <h1 className='story__grid--title'>Building</h1>
           <p className='story__grid--text'>From a very young age I was obsessed with building things. I loved Legos, Duplos, Bionicles and many other building/architectirl toys. As I look back on those years with a bit more wisdom on myself I realized I liked those toys but loved the feeling of building or creating my own original ideas. To this day I feel fulfilled and proud of building and creating beautiful products, designs and experiences.</p>
         </div>
         <div>
-          <h1 className='story__timeline'>Graduate and get a random job at amazon in July 2019</h1>
+          <div className='timeline__button'>
+            <div className='timeline__button--front'>
+              <h1 className='story__timeline'>Graduate and get a random job at amazon in July 2019</h1>
+              </div>
+            </div>
           <DownCarat className='downCarat' />
        </div>
         <div>
