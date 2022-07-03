0a2512abbf7eb09370496964bb69a78f7a6d802d
Noah Hendrickson <noahjames017@gmail.com>
2022-07-02 21:15:01 -0400

    homely page updated

--- src/FooterNav.js
+++ src/FooterNav.js
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom'
 
 const FooterNav = () => {
   return (
-    <div className='FooterNav'>
+  <div className='FooterNav'>
+    <div className='FooterNavSection'>
+        <h1 className='FooterTitle'>Jump to another project</h1>
+    </div>
     <div className='FooterNavSection'>
       <h1 className='FooterTitle'>UX</h1>
     <Link className='FooterLink' to="/HomelyPage">Homely App</Link>
@@ -15,7 +18,6 @@ const FooterNav = () => {
     <Link className='FooterLink' to="/NachoBox">Nacho Box</Link>
     <Link className='FooterLink' to="/FitPage">Fit</Link>
     </div>
-  
   </div>
   )
 }
--- src/UXPages/HomelyAppPage.js
+++ src/UXPages/HomelyAppPage.js
@@ -11,6 +11,7 @@ import Homely3 from "../Assets/NewHomely/Homely-03.svg"
 import Homely4 from "../Assets/NewHomely/Homely-04.svg"
 import Homely5 from "../Assets/NewHomely/Homely-05.svg"
 import Homely6 from "../Assets/HomelyApp/homelyNew-02.svg"
+import Homely7 from "../Assets/NewHomely/HomelyWorkFlow.png"
 
 const HomelyAppPage = () => {
 
@@ -27,8 +28,8 @@ const HomelyAppPage = () => {
             <h1 className='JourneyTitle'>
             The Journey
             </h1>
-              <h2 className='JourneyText'>It began with writing down    all the features we wanted to include and marking their priority. We decided the chat and service ticket features were the highest priority and would see the most traffic.</h2>
-              <h2 className='JourneyText'> I then took those high priorty features and made wireframes and concepts on the general shaping and vibe of the experience.
+              <h2 className='JourneyText'>It began with writing down    all the features we wanted to include and marking their priority. We decided the chat and service ticket features were the highest priority and started as the main focus.</h2>
+              <h2 className='JourneyText'> I took those high priorty features and made wireframes and concepts on the general shaping and vibe of the experience.
               </h2> 
               <h2 className='JourneyText'> This part of the process undergoes a lot of transformation and iteration trying to nail down the finalized look and style of the UI. The inspiration for the design was to be clean, lively and modern.</h2>
           </div>
@@ -37,10 +38,12 @@ const HomelyAppPage = () => {
             <img className="HomelyImage" src={Homely3} />
           </div>
         </div>
-        <div>
+        <div className='ProcessImages--full'>
           <img className="HomelyImage HomelyImageSmaller" src={Homely4} />
           <img className="HomelyImage HomelyImageSmaller" src={Homely5} />
         </div>
+        <h1 className='JourneyTitle2'>When building the user paths I structured the file and screens by feature to help with the design to development process</h1>
+        <img className="HomelySlideImage" src={Homely7} />
         <img className="HomelyImage" src={Homely6} />
       </div>
       <FooterNav />
